ac017c75dfdb06a1cd94e090c75e959b7d63cff5
= <=>
2020-12-16 01:35:52 +0530

    Updated

---
+++ springboot-backend/src/main/java/com/alpha/model/Employee.java
@@ -0,0 +1,73 @@
+package com.alpha.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastname;
+
+    @Column(name = "email_id")
+    private String emailId;
+
+    public Employee() {
+    }
+
+    public Employee(long id, String firstName, String lastname, String emailId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.emailId = emailId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", emailId='" + emailId + '\'' +
+                '}';
+    }
+}
